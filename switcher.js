c611ee7d39b323693d04ef0ecbc7bd3f17b37b6b
Paul Kehrer <pkehrer@hyperion.(none)>
2010-06-26 19:12:06 -0500

    added switching tabs 10-19 courtesy Zaudragon

--- switcher.js
+++ switcher.js
@@ -16,35 +16,67 @@ function handleMessage(event) {
 }
 
 function checkKeyDown(event) {
+	var tab;
 	switch(event.keyCode) {
-		case 49:
-		case 50:
-		case 51:
-		case 52:
-		case 53:
-		case 54:
-		case 55:
-		case 56:
-		case 57:
-		case 58:
-			/*
-			get the current keyCombo again to be safe.
-			I'd rather propagate via the change event but i can't get that to work right now
-			in practice this means if you change the pref you'll have to press the new key combo
-			twice to make it work properly
-			*/
-			if (
-			(keyCombo == 'ctrl' && event.ctrlKey == true) || 
-			(keyCombo == 'opt' && event.altKey == true) || 
-			(keyCombo == 'ctrlopt' && event.altKey == true && event.ctrlKey == true) || 
-			(keyCombo == 'cmd' && event.metaKey == true)
-			) {
-				event.stopPropagation();
-				event.preventDefault();
-				safari.self.tab.dispatchMessage('switchTabs',event.keyCode);
-			}
+		case 49: //1
+		case 50: //2
+		case 51: //3
+		case 52: //4
+		case 53: //5
+		case 54: //6
+		case 55: //7
+		case 56: //8
+		case 57: //9
+			tab = event.keyCode - 49;
+		break;
+		case 48: //0
+			//tabs are zero indexed.
+			tab = 9;
+		break;
+		case 229: //this keycode fires for some reason the first time you push ctrl-q...need to investigate why. for now, let's capture.
+		case 81: //q
+			tab = 10;
+		break;
+		case 87: //w
+			tab = 11;
+		break;
+		case 69: //e
+			tab = 12;
+		break;
+		case 82: //r
+			tab = 13;
+		break;
+		case 84: //t
+			tab = 14;
+		break;
+		case 89: //y
+			tab = 15;
+		break;
+		case 85: //u
+			tab = 16;
+		break;
+		case 73: //i
+			tab = 17;
+		break;
+		case 79: //o
+			tab = 18;
+		break;
+		case 80: //p
+			tab = 19;
 		break;
 		default:
 		//do nothing
+	}
+	if(tab != undefined) {
+		if (
+		(keyCombo == 'ctrl' && event.ctrlKey == true) || 
+		(keyCombo == 'opt' && event.altKey == true) || 
+		(keyCombo == 'ctrlopt' && event.altKey == true && event.ctrlKey == true) || 
+		(keyCombo == 'cmd' && event.metaKey == true)
+		) {
+			event.stopPropagation();
+			event.preventDefault();
+			safari.self.tab.dispatchMessage('switchTabs',tab);
+		}
 	}
 }
